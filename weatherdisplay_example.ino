f899ac5e08ee7ab005d1cfed44453c9b873eb18e
Miles Breslin <milesbreslin@gmail.com>
2018-11-06 16:23:21 -0800

    Seven Segment Display Code

--- weatherdisplay_example.ino
+++ weatherdisplay_example.ino
@@ -26,7 +26,6 @@
 
 #include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
 #include <Adafruit_GFX.h>
-#include <Adafruit_LEDBackpack.h>
 
 #include <WiFi.h>
 #include <AdafruitIO.h>
@@ -53,18 +52,54 @@
 AdafruitIO_Feed *hightemp = io.feed("hightemp"); // set up the 'hightemp' feed
 AdafruitIO_Feed *precipitation = io.feed("precipitation"); // set up the 'precipitation' feed
 
-Adafruit_7segment matrix = Adafruit_7segment();  // create segment display object
+void setSevSeg(int n, int s);
+void setSingleSevSeg(int device, char n);
+void SevSegPowerOff();
+void SevSegPowerOn();
+void initSevSeg();
+
+
+const char SEVSEG_nPINS[] = { 21, 17, 16, 19, 18, 5, 4};
+const char SEVSEG_sPINS[] = { 15, 33, 27, 12};
+const bool SEVSEG_MAP[10][7] = {
+  //     GFEDCBA  Segments      7-segment map:
+  { 0,1,1,1,1,1,1 }, // 0   "0"
+  { 0,0,0,0,1,1,0 }, // 1   "1"
+  { 1,0,1,1,0,1,1 }, // 2   "2"
+  { 1,0,0,1,1,1,1 }, // 3   "3"
+  { 1,1,0,0,1,1,0 }, // 4   "4"
+  { 1,1,0,1,1,0,1 }, // 5   "5"
+  { 1,1,1,1,1,0,1 }, // 6   "6"
+  { 0,0,0,0,1,1,1 }, // 7   "7"
+  { 1,1,1,1,1,1,1 }, // 8   "8"
+  { 1,1,0,1,1,1,1 }  // 9   "9"
+};
 
 void setup() {
-  
-  matrix.begin(0x70); // initialize matrix display
-  
   // start the serial connection
   Serial.begin(115200);
 
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
+  //pin 12 on sevseg is 12 on huzzah, 5 on uno
+  //pin 11 on sevseg is A4 on huzzah, 12 on uno
+  //pin 10 on sevseg is A5 on huzzah, 8 on uno
+  //pin 9 on sevseg is SCK on huzzah, 13 on uno
+  //pin 8 on sevseg is MO on huzzah, 9 on uno
+  //pin 7 on sevseg is MI on huzzah, 10 on uno
+  //pin 6 on sevseg is MO on huzzah, 7 on uno
+  //pin 5 on sevseg is 27 on huzzah, 4 on uno
+  //pin 4 on sevseg is 33 on huzzah, 3 on uno
+  //pin 3 on sevseg is  on huzzah, 11 on uno
+  //pin 2 on sevseg is  on huzzah, 6 on uno
+  //pin 1 on sevseg is 15 on huzzah, 2 on uno
+
+  //sevseg.Begin(1,15,33,27,12,6,7,8,9,10,11,12,13);
+  //sevseg.Brightness(90);
+  //matrix.print(10, DEC);  // send the temperature value to the display
+  //matrix.writeDisplay();          // light up display
+  initSevSeg();
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
 
   Serial.print("Connecting to Adafruit IO");
 
@@ -77,11 +112,27 @@ void setup() {
   // received from adafruit io.
   hightemp->onMessage(handleTemp);
   precipitation->onMessage(handleCondition);
-
+  setSevSeg(9999, 10);
   // wait for a connection
-  while(io.status() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
+  {     // animation while waiting for connection; removes need for serial connectivity
+    SevSegPowerOff();
+    for (int i=0; i<4; i++) {
+      digitalWrite(SEVSEG_sPINS[i], 0);
+      delay(500);
+    }
+    int i = 0;
+    while(io.status() < AIO_CONNECTED) {
+      Serial.print(".");
+      digitalWrite(SEVSEG_nPINS[i%7], ((i/7)+1) %2);
+      delay(500);
+      i++;
+    }
+    for (int i=0; i<10; i++) {
+      SevSegPowerOn();
+      delay(250);
+      SevSegPowerOff();
+      delay(250);
+    }
   }
 
   // we are connected
@@ -108,9 +159,7 @@ void handleTemp(AdafruitIO_Data *data) {
   Serial.println(data->value());  // print the temperature data to the serial monitor
   
   int todaysHigh = data->toInt(); // store the incoming temperature data as an integer
-  matrix.print(todaysHigh, DEC);  // send the temperature value to the display
-  matrix.writeDisplay();          // light up display
-  delay(500);                     // wait half a second
+  setSevSeg(todaysHigh, 30); // set the display for 30 second
 }  
   
 void handleCondition(AdafruitIO_Data *data) {
@@ -161,4 +210,54 @@ void handleCondition(AdafruitIO_Data *data) {
     digitalWrite(LED_GREEN, 1);
     digitalWrite(LED_BLUE, 1);
    }
+}
+
+void setSevSeg(int n, int s) {
+  const int onMS = 2;
+  if ( n < 10000 && n > 0 ){
+    char num[] = { n % 10,
+                    n /10 %10,
+                    n /100 %10,
+                    n /1000 };
+    for (int i=0; i<s*(1000/onMS); i++) {
+      setSingleSevSeg(i%4, num[i%4]);
+      delay(onMS);
+    }
+  }
+}
+
+void setSingleSevSeg(int device, char n) {
+  SevSegPowerOff();
+  for (int i=0; i<7; i++) {
+    digitalWrite(SEVSEG_nPINS[i], SEVSEG_MAP[n][i]);
+  }
+  digitalWrite(SEVSEG_sPINS[device], 0);
+}
+
+void SevSegPowerOff() {
+  for (int i=0; i<4; i++) {
+    digitalWrite(SEVSEG_sPINS[i], 1);
+  }
+  for (int i=0; i<7; i++) {
+    digitalWrite(SEVSEG_nPINS[i], 0);
+  }
+}
+
+void SevSegPowerOn() {
+  for (int i=0; i<4; i++) {
+    digitalWrite(SEVSEG_sPINS[i], 0);
+  }
+  for (int i=0; i<7; i++) {
+    digitalWrite(SEVSEG_nPINS[i], 1);
+  }
+}
+
+void initSevSeg() {
+  for (int i=0; i<4; i++) {
+    pinMode(SEVSEG_sPINS[i], OUTPUT);
+  }
+  for (int i=0; i<7; i++) {
+    pinMode(SEVSEG_nPINS[i], OUTPUT);
+  }
+  SevSegPowerOff();
 }
